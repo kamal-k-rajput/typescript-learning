0b84252a5da86d7086e2c56bf6a950f5bde112a7
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2024-05-30 00:25:34 +0530

    Generics example

--- typescript_learning/generics.ts
+++ typescript_learning/generics.ts
@@ -1,3 +1,47 @@
-let s: string = "kamal";
+// When to use generics
 
-let x: string = "amit";
+interface IAnimal {
+  name: string;
+}
+
+interface IHuman {
+  firstName: string;
+  lastName: string;
+}
+
+export const getDisplayName = <TItem extends IAnimal | IHuman>(
+  item: TItem,
+): TItem extends IHuman ? { name: string } : { name: string } => {
+  if ("name" in item) {
+    return { name: item.name };
+  } else {
+    return { name: item.firstName + " " + item.lastName };
+  }
+};
+
+class Animal implements IAnimal {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Human implements IHuman {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+let item1 = new Human("amit", "kumar");
+
+let item2 = new Animal("dog");
+
+let n_one = getDisplayName(item1);
+
+let n_two = getDisplayName(item2);
+
+console.log(n_one);
+console.log(n_two);
